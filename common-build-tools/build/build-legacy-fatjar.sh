2a9652161086bb84c4420c50035e426cf449b4ce
Ville Seppänen <ville.seppanen@gofore.com>
2018-08-15 16:56:53 +0300

    Docker push

--- common-build-tools/build/build-legacy-fatjar.sh
+++ common-build-tools/build/build-legacy-fatjar.sh
@@ -2,7 +2,7 @@
 
 export ARTIFACT_NAME=$1
 export ECR_REPO="190073735177.dkr.ecr.eu-west-1.amazonaws.com/utility"
-export DOCKER_TARGET="$ECR_REPO/${ARTIFACT_NAME}:${TRAVIS_BUILD_NUMBER}"
+export DOCKER_TARGET="${ECR_REPO}/${ARTIFACT_NAME}:${TRAVIS_BUILD_NUMBER}"
 export DOCKER_BUILD_DIR="/tmp/docker_build"
 
 rm -rf ${DOCKER_BUILD_DIR}/target
@@ -17,6 +17,4 @@ sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/target/D
 
 docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}/target
 docker images
-
-#- docker tag $APP_NAME:$TRAVIS_BUILD_ID $AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$APP_NAME:$TRAVIS_BUILD_ID
-#- docker push $AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$APP_NAME:$TRAVIS_BUILD_ID
+docker push ${DOCKER_TARGET}
