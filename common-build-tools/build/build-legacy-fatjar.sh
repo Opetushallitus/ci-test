05f12dfbfc660d301008a3dc8f8a61823ab1e174
Ville Seppänen <ville.seppanen@gofore.com>
2018-08-15 15:16:37 +0300

    Clone ci-tools repo, use build number instead of job number

--- common-build-tools/build/build-legacy-fatjar.sh
+++ common-build-tools/build/build-legacy-fatjar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 export ARTIFACT_NAME=$1
+export DOCKER_TARGET="$ECR_REPO/${ARTIFACT_NAME}:${TRAVIS_BUILD_NUMBER}"
 export DOCKER_BUILD_DIR="/tmp/docker_build"
 
 rm -rf ${DOCKER_BUILD_DIR}/target
@@ -12,9 +13,6 @@ cp common-build-tools/build/Dockerfile ${DOCKER_BUILD_DIR}/target/
 cp common-build-tools/run/run-legacy-fatjar.sh ${DOCKER_BUILD_DIR}/target/run
 
 sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/target/Dockerfile
-
-export IMAGE_NAME=`echo "${ARTIFACT_NAME}-${TRAVIS_BRANCH}" | sed -e "s/-$//" | tr '[:upper:]' '[:lower:]'`
-export DOCKER_TARGET="$ECR_REPO/${IMAGE_NAME}:${TRAVIS_JOB_NUMBER}"
 
 docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}/target
 docker images
