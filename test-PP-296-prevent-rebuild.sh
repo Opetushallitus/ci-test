b95317e8a4bf7df6348e33d649e6096284dbb596
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-12 13:38:20 +0200

    Fix

--- test-PP-296-prevent-rebuild.sh
+++ test-PP-296-prevent-rebuild.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -o
+
 # TRAVIS_BUILD_NUMBER="560"
 # BUILD_ID="ci-$TRAVIS_BUILD_NUMBER"
 # SERVICE="koulutusinformaatio-indeksoija"
 
-echo "Checking if current build ${BUILD_ID} already exists in builds table"
-RESPONSE=$(aws dynamodb get-item --table-name builds --key "{\"Service\": {\"S\": \"$SERVICE\"}, \"Build\": {\"S\": \"$BUILD_ID\"}}" --output json)
-FOUND_ID=$(echo $RESPONSE|jq -r ".Item.Build.S")
+SERVICE="ci-test"
+BUILD_ID="ci-${TRAVIS_BUILD_NUMBER}"
 
-if [[ $FOUND_ID == $BUILD_ID ]]; then
+echo "Checking if current build ${BUILD_ID} already exists in builds table"
+PREVIOUS_BUILD=$(aws dynamodb get-item --table-name builds --key "{\"Service\": {\"S\": \"$SERVICE\"}, \"Build\": {\"S\": \"$BUILD_ID\"}}" --output json --region eu-west-1)
+PREVIOUS_BUILD_ID=$(echo $RESPONSE|jq -r ".Item.Build.S")
+
+if [[ $PREVIOUS_BUILD_ID == $BUILD_ID ]]; then
     echo "This seems to be a rebuild!"
     terminate_build
 else
