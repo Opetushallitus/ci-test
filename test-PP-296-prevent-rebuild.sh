79bab2ee45ac27b78a578ed06328c02c1ea5fc6d
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-18 13:12:13 +0200

    Fix check

--- test-PP-296-prevent-rebuild.sh
+++ test-PP-296-prevent-rebuild.sh
@@ -1,19 +1,12 @@
 #!/bin/bash -e
 
-set -o
-
-# TRAVIS_BUILD_NUMBER="560"
-# BUILD_ID="ci-$TRAVIS_BUILD_NUMBER"
-# SERVICE="koulutusinformaatio-indeksoija"
-
+# TRAVIS_BUILD_NUMBER="50"
 SERVICE="ci-test"
 BUILD_ID="ci-${TRAVIS_BUILD_NUMBER}"
 
 echo "Checking if current build ${BUILD_ID} already exists in builds table"
 PREVIOUS_BUILD=$(aws dynamodb get-item --table-name builds --key "{\"Service\": {\"S\": \"$SERVICE\"}, \"Build\": {\"S\": \"$BUILD_ID\"}}" --output json --region eu-west-1)
-echo "Received response: $PREVIOUS_BUILD"
-PREVIOUS_BUILD_ID=$(echo $RESPONSE|jq -r ".Item.Build.S")
-echo "Parsed ID: $PREVIOUS_BUILD_ID"
+PREVIOUS_BUILD_ID=$(echo $PREVIOUS_BUILD|jq -r ".Item.Build.S")
 
 if [[ $PREVIOUS_BUILD_ID == $BUILD_ID ]]; then
     echo "This seems to be a rebuild!"
